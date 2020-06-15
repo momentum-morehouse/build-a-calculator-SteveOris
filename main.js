440967dcc4a1e652daedb8718ddb44f8121ffeee
Steve Orisakwe <Steveoris@parkrowmedia.com>
2020-06-15 16:03:58 +0000

    first push

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
+function answer(value) {
+  document.getElementById("results").value+=value
+}
+function solve(){
+let x = document.getElementById("results").value
+let y = eval(x)
+document.getElementById("results").value = y 
+}
+function clear(){
+ document.getElementById("results").value = ""
+}
 
